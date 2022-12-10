8283d804cd3c172d861ccf26e812b2b1af71be9d
Puneet Tiwari <puneet.tiwari@sap.com>
2022-12-10 17:48:59 +0530

    update mongoATLAS url Added dotenv package

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
 // const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var dbUrl = 'mongodb+srv://mlabsu:mlabpass@cluster0.3dkfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+var dbUrl = 'mongodb+srv://mlabsu:mlabpass@cluster0.xbnneze.mongodb.net/?retryWrites=true&w=majority'
 
 var Message = mongoose.model('Message', {
     name: String,
@@ -50,6 +51,6 @@ mongoose.connect(dbUrl, (err) => {
     console.log('mongo db connection', err)
 })
 
-var server = http.listen(3000, () => {
+var server = http.listen(process.env.PORT || 3000, () => {
     console.log('server is listening on port', server.address().port)
 })
